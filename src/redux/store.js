0e9b7b2521dd6ea6940e559501c0a0218bc9a60c
Anna Kovalchuk <annazukh@gmail.com>
2023-01-04 23:06:51 +0200

    add_redux

---
+++ src/redux/store.js
@@ -0,0 +1,24 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { filterReducer } from './filterSlice';
+import { contactsReducer } from './contactsSlice';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const rootReducer = combineReducers({
+  contacts: contactsReducer,
+  filter: filterReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
